8a66894c1afb0e5537e4802b933aa2e770e31706
purplelightning <1979506141@qq.com>
2017-06-18 13:06:13 +0800

    下拉刷新,上拉加载

--- app/src/main/java/com/example/wind/recyclerviewexecise/RanimationActivity.java
+++ app/src/main/java/com/example/wind/recyclerviewexecise/RanimationActivity.java
@@ -1,6 +1,7 @@
 package com.example.wind.recyclerviewexecise;
 
 import android.app.Activity;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -20,7 +21,10 @@ import java.util.List;
 public class RanimationActivity extends AppCompatActivity {
 
     private Fruit apple, banana, grape, orange, pineapple, watermelon, pear, cherry, mango,
-            strawberry, cherry1;
+            strawberry, cherry1,mango1,grape1;
+
+    private SwipeRefreshLayout mRefreshLayout;
+    private LinearLayoutManager mLinearLayoutManager;
 
     //数组初始化！
     private List<Fruit> fruitList = new ArrayList<>();
@@ -38,6 +42,7 @@ public class RanimationActivity extends AppCompatActivity {
         //原来的工具栏不显示
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
+        mRefreshLayout=(SwipeRefreshLayout) findViewById(R.id.layout_swiperefresh);
 
         recyclerView = (RecyclerView) findViewById(R.id.rec_list);
         initData();
@@ -45,20 +50,19 @@ public class RanimationActivity extends AppCompatActivity {
         mAdapter = new FruitAdapter(fruitList);
         recyclerView.setAdapter(mAdapter);
 
-        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+        mLinearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(mLinearLayoutManager);
 
         //下拉列表1设置recyclerview布局
         Spinner spinner1 = (Spinner) findViewById(R.id.spinner1);
 
-
-        //增加,减少item监听器
+        //Add,Del的监听器,增加,减少item
         findViewById(R.id.add).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mAdapter.add(cherry1, 2);
             }
         });
-
         findViewById(R.id.del).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -66,7 +70,28 @@ public class RanimationActivity extends AppCompatActivity {
             }
         });
 
+        //下拉监听
+        mRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                //在list最前面加入一条数据
+                fruitList.add(mango1);
+                //数据重新加载完成后，提示数据发生改变，并且设置现在不在刷新
+                mAdapter.notifyDataSetChanged();
+                mRefreshLayout.setRefreshing(false);
+            }
+        });
 
+        /**上拉监听
+         * 监听addOnScrollListener这个方法，新建EndLessOnScrollListener
+         * 在onLoadMore方法中去完成上拉加载的操作
+         * */
+        recyclerView.addOnScrollListener(new EndLessOnScrollListener(mLinearLayoutManager){
+            @Override
+            public void onLoadMore(int currentPage){
+                loadMoreData();
+            }
+        });
     }
 
     @Override
@@ -96,6 +121,16 @@ public class RanimationActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    //每次上拉加载的时候，就加载十条数据到RecyclerView中
+    private void loadMoreData(){
+        for(int i=0;i<5;i++){
+            fruitList.add(grape1);
+            mAdapter.notifyDataSetChanged();
+
+        }
+    }
+
+    //初始化数据
     private void initData() {
         apple = new Fruit("apple", R.drawable.apple_pic);
         banana = new Fruit("banana", R.drawable.banana_pic);
@@ -109,8 +144,11 @@ public class RanimationActivity extends AppCompatActivity {
         watermelon = new Fruit("watermelon", R.drawable.watermelon_pic);
 
         cherry1 = new Fruit("New Item", R.drawable.cherry_pic);
+        mango1=new Fruit("下拉的芒果",R.drawable.mango_pic);
+        grape1=new Fruit("上拉的葡萄",R.drawable.grape_pic);
 
-        for (int i = 0; i < 3; i++) {
+
+        for (int i = 0; i < 1; i++) {
             fruitList.add(apple);
             fruitList.add(banana);
             fruitList.add(cherry);
